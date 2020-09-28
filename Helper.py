6f38f8a9ebeaa83d23ef4ffed84f0bb5bdfdfea0
beshenovanton <72032024+beshenovanton@users.noreply.github.com>
2020-09-28 18:16:23 +0300

    Update and rename Helper.py to Try.py

--- Helper.py
+++
@@ -1,7 +0,0 @@
-import NeuroNetLibrary
-
-nv.say(hello, val = None)
-
-nv.listen()
-
-r = nlu.extract('Да я хочу cashback', 'increase_cashback')
---
+++ Try.py
@@ -0,0 +1,9 @@
+import NeuroNetLibrary
+
+nv.say(hello, val = None)
+
+nv.listen()
+
+r = nlu.extract('Да я хочу cashback', 'increase_cashback')
+
+                      ## Не смог разобраться до конца как работает логака библиотеки NeuroNet
